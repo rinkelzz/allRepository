c182553be83cfdd2fed88395cd11c706390eeb24
rinkelzz <55958954+rinkelzz@users.noreply.github.com>
2025-10-04 23:42:02 +0100

    Add configuration-driven friendly GitHub explorer

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'owner' => 'symfony',
+    'default_repository' => 'symfony',
+    'title' => 'Freundlicher GitHub Repository Explorer',
+    'welcome_message' => 'Willkommen! Entdecke die Projekte dieses GitHub-Owners.',
+];
